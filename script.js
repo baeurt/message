2edc6f9b5b16e3fcf0a648bc2edc3a564a2c3682
baeurt <158698848+baeurt@users.noreply.github.com>
2024-02-04 16:39:27 +0800

    yams

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
-const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no-btn");
+const yesBtn = document.querySelector(".yes :o-btn");
+const noBtn = document.querySelector(".no grrr-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "yay, see you on the 17th! :p";
+  question.innerHTML = "yay, see you! love u :p";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
